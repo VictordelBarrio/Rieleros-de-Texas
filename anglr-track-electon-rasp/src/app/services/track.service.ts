ace161ee88e9830f417e17b06cba47a43940cbef
VictorElGringo <78885228+VictorElGringo@users.noreply.github.com>
2021-11-27 12:52:45 -0600

    corrected routes

--- anglr-track-electon-rasp/src/app/services/track.service.ts
+++ anglr-track-electon-rasp/src/app/services/track.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackService {
+  idUser: any;
 
-  constructor(private afs: AngularFirestore) { }
+  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {
+    this.afAuth.currentUser.then((result)=>{
+      this.idUser = result?.uid;
+      console.log(this.idUser)
+    }); 
+  }
 
   saveBusLocation(lat: number, long: number, routeName: string): Promise<any> {
     return this.afs.collection('/locationBus').doc('TacoBus').collection('/listRoutes').doc(routeName)
